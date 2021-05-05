5e8f34d4e13b068c7b6a492d073cc84c88180305
onsenkozo <masamic@masamic.net>
2021-05-05 17:45:27 +0900

    Add More Commands

--- mkbin_mw145/MW145.cs
+++ mkbin_mw145/MW145.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 /// <summary>
 /// 
 /// </summary>
@@ -14,20 +15,29 @@ namespace mkbin_mw145
         /// </summary>
         public MW145()
         {
-
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
 
         // ========================================================
         // 定数・Enumerate
         // ========================================================
-        public const int UNDERLINE     = 128;
-        public const int ITALIC        = 64;
-        public const int DOUBLE_WIDTH  = 32;
-        public const int DOUBLE_HEIGHT = 16;
-        public const int BOLD          = 8;
-        public const int HALF_WIDTH    = 4;
-        public const int PROPOTIONAL   = 2;
-        public const int CPI_12        = 1;
+        public const int ANK_UNDERLINE     = 128;
+        public const int ANK_ITALIC        = 64;
+        public const int ANK_DOUBLE_WIDTH  = 32;
+        public const int ANK_DOUBLE_HEIGHT = 16;
+        public const int ANK_BOLD          = 8;
+        public const int ANK_HALF_WIDTH    = 4;
+        public const int ANK_PROPOTIONAL   = 2;
+        public const int ANK_CPI_12        = 1;
+
+        public const int KNJ_UNDERLINE        = 128;
+        public const int KNJ_ITALIC           = 64;
+        public const int KNJ_SUBSCRIPT        = 32;
+        public const int KNJ_QUATER_SIZE      = 16;
+        public const int KNJ_DOUBLE_HEIGHT    = 8;
+        public const int KNJ_DOUBLE_WIDTH     = 4;
+        public const int KNJ_HALF_WIDTH       = 2;
+        public const int KNJ_VERTICAL_WRITING = 1;
 
         // ========================================================
         // 制御文字
@@ -206,6 +216,93 @@ namespace mkbin_mw145
         }
 
         /// <summary>
+        /// 絶対垂直位置指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetVerticalAbsolutePosition(int value)
+        {
+            if (0 <= value &&
+                value < 1180)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x28);
+                binaryWriter.Write((byte)0x56);
+                binaryWriter.Write((byte)0x02);
+                binaryWriter.Write((byte)0x00);
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetVerticalAbsolutePosition: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 相対垂直位置指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetVerticalReativePosition(int value)
+        {
+            if (0 <= value &&
+                value < 1180)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x28);
+                binaryWriter.Write((byte)0x76);
+                binaryWriter.Write((byte)0x02);
+                binaryWriter.Write((byte)0x00);
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetVerticalReativePosition: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// ページフォーマット設定
+        /// </summary>
+        /// <param name="topMargin"></param>
+        /// <param name="bottomMargin"></param>
+        protected void SetPageFormat(int topMargin, int bottomMargin)
+        {
+            if (0 <= topMargin && 
+                topMargin < 1180 &&
+                0 <= bottomMargin &&
+                bottomMargin < 1180 &&
+                topMargin < bottomMargin)
+            {
+                byte tlb = (byte)(topMargin % 255);
+                byte thb = (byte)(topMargin / 256);
+
+                byte blb = (byte)(bottomMargin % 255);
+                byte bhb = (byte)(bottomMargin / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x28);
+                binaryWriter.Write((byte)0x63);
+                binaryWriter.Write((byte)0x04);
+                binaryWriter.Write((byte)0x00);
+                binaryWriter.Write(tlb);
+                binaryWriter.Write(thb);
+                binaryWriter.Write(blb);
+                binaryWriter.Write(bhb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetPageFormat: Bad Parameter: topMargin {0:d} smaller than bottomMargin {1:d}", topMargin, bottomMargin));
+            }
+        }
+
+        /// <summary>
         /// アンダーライン指定/解除
         /// </summary>
         protected void SetUnderline(bool value)
@@ -281,10 +378,43 @@ namespace mkbin_mw145
         }
 
         /// <summary>
+        /// 垂直タブ設定
+        /// </summary>
+        /// <param name="values"></param>
+        protected void SetVerticalTabStops(params int[] values)
+        {
+            if (values.Length < 17)
+            {
+                int MinValue = int.MinValue;
+                foreach (int val in values)
+                {
+                    if (MinValue > val)
+                    {
+                        throw new Exception(string.Format("SetVerticalTabStops: Bad Parameter Sequence: Current Value {0:d} is smaller than Previous Value {1:d}", val, MinValue));
+                    }
+                }
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x42);
+
+                foreach (int val in values)
+                {
+                    binaryWriter.Write((byte)val);
+                }
+
+                binaryWriter.Write((byte)0x0);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetVerticalTabStops: Bad Parameter Count: {0:d}", values.Length));
+            }
+        }
+
+        /// <summary>
         /// 水平タブ設定
         /// </summary>
         /// <param name="values"></param>
-        protected void SetTabStops(params int[] values)
+        protected void SetHorizontalTabStops(params int[] values)
         {
             if (values.Length < 33)
             {
@@ -293,7 +423,7 @@ namespace mkbin_mw145
                 {
                     if (MinValue > val)
                     {
-                        throw new Exception(string.Format("SetTabStops: Bad Parameter Sequence: Current Value {0:d} is smaller than Previous Value {1:d}", val, MinValue));
+                        throw new Exception(string.Format("SetHorizontalTabStops: Bad Parameter Sequence: Current Value {0:d} is smaller than Previous Value {1:d}", val, MinValue));
                     }
                 }
 
@@ -309,7 +439,7 @@ namespace mkbin_mw145
             }
             else
             {
-                throw new Exception(string.Format("SetTabStops: Bad Parameter Count: {0:d}", values.Length));
+                throw new Exception(string.Format("SetHorizontalTabStops: Bad Parameter Count: {0:d}", values.Length));
             }
         }
 
@@ -540,6 +670,387 @@ namespace mkbin_mw145
             {
                 binaryWriter.Write('0');
             }
+        }
+
+        /// <summary>
+        /// ダウンロードデータプリント
+        /// </summary>
+        /// <param name="value"></param>
+        protected void PrintPreDownloadedData(int value)
+        {
+            if (0 <= value && value <= 98)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x69);
+                binaryWriter.Write((byte)0x46);
+                binaryWriter.Write((byte)0x50);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("PrintPreDownloadedData: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// プリンタステータス要求
+        /// </summary>
+        protected void RequestPrinterStatus()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x69);
+            binaryWriter.Write((byte)0x53);
+        }
+
+        /// <summary>
+        /// コマンドモード切替
+        /// </summary>
+        /// <param name="value"></param>
+        protected void ChangeCommandMode(int value)
+        {
+            if (0 == value || 1 == value || 3 == value)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x69);
+                binaryWriter.Write((byte)0x61);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("ChangeCommandMode: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// ランドスケープ設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetLandscapeMode(bool value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x69);
+            binaryWriter.Write((byte)0x4c);
+            if (value)
+            {
+                binaryWriter.Write('1');
+            }
+            else
+            {
+                binaryWriter.Write('0');
+            }
+        }
+
+        // ========================================================
+        // FSコマンド
+        // ========================================================
+
+        /// <summary>
+        /// 漢字モード指定
+        /// </summary>
+        protected void SetKanjiMode()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x26);
+        }
+
+        /// <summary>
+        /// 漢字モード解除
+        /// </summary>
+        protected void UnsetKanjiMode()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x2e);
+        }
+
+        /// <summary>
+        /// 半角文字縦書き2文字印字
+        /// </summary>
+        /// <param name="char1"></param>
+        /// <param name="char2"></param>
+        protected void Print2HalfAs1FullWidth(char char1, char char2)
+        {
+            var sjis = Encoding.GetEncoding("shift_jis");
+            byte[] sjis1 = sjis.GetBytes(char1.ToString());
+            byte[] sjis2 = sjis.GetBytes(char2.ToString());
+
+            byte llb = 0x00;
+            byte lhb = 0x00;
+
+            byte rlb = 0x00;
+            byte rhb = 0x00;
+
+            if (sjis1.Length < 2)
+            {
+                llb = 0x00;
+                lhb = sjis1[0];
+            }
+            else
+            {
+                llb = sjis1[0];
+                lhb = sjis1[1];
+            }
+
+            if (sjis2.Length < 2)
+            {
+                rlb = 0x00;
+                rhb = sjis2[0];
+            }
+            else
+            {
+                rlb = sjis2[0];
+                rhb = sjis2[1];
+            }
+
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x44);
+            binaryWriter.Write((byte)llb);
+            binaryWriter.Write((byte)lhb);
+            binaryWriter.Write((byte)rlb);
+            binaryWriter.Write((byte)rhb);
+        }
+
+        /// <summary>
+        /// 縦書き指定
+        /// </summary>
+        protected void SetVerticalWriting()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x4a);
+        }
+
+        /// <summary>
+        /// 横書き指定
+        /// </summary>
+        protected void SetHorizontalWriting()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x4b);
+        }
+
+        /// <summary>
+        /// 全角文字のスペース量設定
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        protected void SetFullWidthSpacing(int left, int right)
+        {
+            if (left < 0 || 128 <= left)
+            {
+                throw new Exception(string.Format("SetFullWidthSpacing: Bad Parameter: left {0:d}", left));
+            }
+            else  if (left < 0 || 128 <= left)
+            {
+                throw new Exception(string.Format("SetFullWidthSpacing: Bad Parameter: right {0:d}", right));
+            }
+            else
+            {
+                binaryWriter.Write((byte)0x1c);
+                binaryWriter.Write((byte)0x53);
+                binaryWriter.Write((byte)left);
+                binaryWriter.Write((byte)right);
+            }
+        }
+
+        /// <summary>
+        /// 半角文字のスペース量設定
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        protected void SetHalfWidthSpacing(int left, int right)
+        {
+            if (left < 0 || 128 <= left)
+            {
+                throw new Exception(string.Format("SetHalfWidthSpacing: Bad Parameter: left {0:d}", left));
+            }
+            else if (left < 0 || 128 <= left)
+            {
+                throw new Exception(string.Format("SetHalfWidthSpacing: Bad Parameter: right {0:d}", right));
+            }
+            else
+            {
+                binaryWriter.Write((byte)0x1c);
+                binaryWriter.Write((byte)0x54);
+                binaryWriter.Write((byte)left);
+                binaryWriter.Write((byte)right);
+            }
+        }
+
+        /// <summary>
+        /// 半角文字間スペース補正
+        /// </summary>
+        protected void SetAdjustmentHalfWidthFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x55);
+        }
+
+        /// <summary>
+        /// 半角文字間スペース補正の解除
+        /// </summary>
+        protected void UnsetAdjustmentHalfWidthFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x56);
+        }
+
+        /// <summary>
+        /// 4倍角文字選択
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetDoubleWidthAndHeightFont(bool value)
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x55);
+
+            if (value)
+            {
+                binaryWriter.Write('1');
+            }
+            else
+            {
+                binaryWriter.Write('0');
+            }
+        }
+
+        /// <summary>
+        /// 漢字サイズ指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetKanjiFontSize(int value)
+        {
+            if (value == 16 ||
+                value == 24 ||
+                value == 32 ||
+                value == 33 ||
+                value == 38 ||
+                value == 42 ||
+                value == 46 ||
+                value == 50 ||
+                value == 58 ||
+                value == 67 ||
+                value == 75 ||
+                value == 83 ||
+                value == 92 ||
+                value == 100 ||
+                value == 117 ||
+                value == 133 ||
+                value == 150 ||
+                value == 167 ||
+                value == 200 ||
+                value == 233 ||
+                value == 267 ||
+                value == 300 ||
+                value == 333 ||
+                value == 367 ||
+                value == 400)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1c);
+                binaryWriter.Write((byte)0x59);
+                binaryWriter.Write('0');
+                binaryWriter.Write('0');
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+                binaryWriter.Write('0');
+                binaryWriter.Write('0');
+            }
+            else
+            {
+                throw new Exception(string.Format("SetKanjiFontSize: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 1/4角文字指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetQuaterSquareCharacter(bool value)
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x72);
+
+            if (value)
+            {
+                binaryWriter.Write('1');
+            }
+            else
+            {
+                binaryWriter.Write('0');
+            }
+        }
+
+        /// <summary>
+        /// 漢字アンダーライン指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetKaniUnderline(int value)
+        {
+            if (0 <= value &&
+                value < 5)
+            {
+                binaryWriter.Write((byte)0x1c);
+                binaryWriter.Write((byte)0x2d);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetKaniUnderline: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 漢字印字モードの設定
+        /// </summary>
+        protected void SetKanjiFontStyle(int value)
+        {
+            if (0 <= value && value <= 255)
+            {
+                binaryWriter.Write((byte)0x1c);
+                binaryWriter.Write((byte)0x21);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetKanjiFontStyle: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 半角文字指定
+        /// </summary>
+        protected void SetHalfWidthKanjiFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x0f);
+        }
+
+        /// <summary>
+        /// 半角文字解除
+        /// </summary>
+        protected void UnsetHalfWidthKanjiFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x12);
+        }
+
+        /// <summary>
+        /// 自動解除付き倍角文字指定
+        /// </summary>
+        protected void SetAutoUnsetDoubleWidthKanjiFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x0e);
+        }
+
+        /// <summary>
+        /// 自動解除付き倍角文字解除
+        /// </summary>
+        protected void UnsetAutoUnsetDoubleWidthKanjiFont()
+        {
+            binaryWriter.Write((byte)0x1c);
+            binaryWriter.Write((byte)0x14);
         }
     }
 }
