0a940641c927986bed1b57f28dd5fde34cda43fb
onsenkozo <masamic@masamic.net>
2021-05-05 16:00:21 +0900

    Add More Commands

--- mkbin_mw145/MW145.cs
+++ mkbin_mw145/MW145.cs
@@ -1,4 +1,5 @@
-﻿/// <summary>
+﻿using System;
+/// <summary>
 /// 
 /// </summary>
 namespace mkbin_mw145
@@ -9,12 +10,24 @@ namespace mkbin_mw145
     class MW145 : PrinterCommon
     {
         /// <summary>
-        /// 
+        /// コンストラクタ
         /// </summary>
         public MW145()
         {
 
         }
+
+        // ========================================================
+        // 定数・Enumerate
+        // ========================================================
+        public const int UNDERLINE     = 128;
+        public const int ITALIC        = 64;
+        public const int DOUBLE_WIDTH  = 32;
+        public const int DOUBLE_HEIGHT = 16;
+        public const int BOLD          = 8;
+        public const int HALF_WIDTH    = 4;
+        public const int PROPOTIONAL   = 2;
+        public const int CPI_12        = 1;
 
         // ========================================================
         // 制御文字
@@ -76,6 +89,34 @@ namespace mkbin_mw145
             binaryWriter.Write((byte)0x0f);
         }
 
+        /// <summary>
+        /// 縮小の解除
+        /// </summary>
+        protected void DC2()
+        {
+            binaryWriter.Write((byte)0x12);
+        }
+
+        /// <summary>
+        /// 自動解除付き倍幅拡大の解除
+        /// </summary>
+        protected void DC4()
+        {
+            binaryWriter.Write((byte)0x14);
+        }
+
+        // ========================================================
+        // 特殊処理
+        // ========================================================
+
+        /// <summary>
+        /// スリープの実行
+        /// </summary>
+        protected void Sleep(int millisec)
+        {
+            System.Threading.Thread.Sleep(millisec);
+        }
+
         // ========================================================
         // エスケープコマンド
         // ========================================================
@@ -87,6 +128,127 @@ namespace mkbin_mw145
         {
             binaryWriter.Write((byte)0x1b);
             binaryWriter.Write((byte)0x40);
+        }
+
+        /// <summary>
+        /// 自動解除付き拡大指定
+        /// </summary>
+        protected void AutoResetDoubleWidthFont()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x0e);
+        }
+
+        /// <summary>
+        /// 縮小の指定
+        /// </summary>
+        protected void SetHalfWidthFont()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x0f);
+        }
+
+        /// <summary>
+        /// ANK文字のスペース量設定
+        /// </summary>
+        protected void SetAnkFontSpace(int value)
+        {
+            if (0 <= value && value <= 127)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x20);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetAnkFontSpace: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 一括指定
+        /// </summary>
+        protected void SetFontStyle(int value)
+        {
+            if (0 <= value && value <= 255)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x21);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetFontStyle: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 絶対水平位置指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetHorizontalAbsolutePosition(int value)
+        {
+            if (0 <= value &&
+                value < 1180)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x24);
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetAnkFontSize: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// アンダーライン指定/解除
+        /// </summary>
+        protected void SetUnderline(bool value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x2d);
+            if (value)
+            {
+                binaryWriter.Write('1');
+            }
+            else
+            {
+                binaryWriter.Write('0');
+            }
+        }
+
+        /// <summary>
+        /// 1/8インチ改行量設定
+        /// </summary>
+        protected void SetOneEighthLineHeight()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x30);
+        }
+
+        /// <summary>
+        /// 1/6インチ改行量設定
+        /// </summary>
+        protected void SetOneSixthLineHeight()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x32);
+        }
+
+        /// <summary>
+        /// 最小単位の改行量設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetMinimumLineHeight(int value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x33);
+            binaryWriter.Write((byte)value);
         }
 
         /// <summary>
@@ -105,6 +267,50 @@ namespace mkbin_mw145
         {
             binaryWriter.Write((byte)0x1b);
             binaryWriter.Write((byte)0x35);
+        }
+
+        /// <summary>
+        /// n/60インチ改行量設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetNSixtiethLineHeight(int value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x40);
+            binaryWriter.Write((byte)value);
+        }
+
+        /// <summary>
+        /// 水平タブ設定
+        /// </summary>
+        /// <param name="values"></param>
+        protected void SetTabStops(params int[] values)
+        {
+            if (values.Length < 33)
+            {
+                int MinValue = int.MinValue;
+                foreach (int val in values)
+                {
+                    if (MinValue > val)
+                    {
+                        throw new Exception(string.Format("SetTabStops: Bad Parameter Sequence: Current Value {0:d} is smaller than Previous Value {1:d}", val, MinValue));
+                    }
+                }
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x44);
+
+                foreach (int val in values)
+                {
+                    binaryWriter.Write((byte)val);
+                }
+
+                binaryWriter.Write((byte)0x0);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetTabStops: Bad Parameter Count: {0:d}", values.Length));
+            }
         }
 
         /// <summary>
@@ -144,12 +350,22 @@ namespace mkbin_mw145
         }
 
         /// <summary>
-        /// パイカピッチ指定
+        /// 順方向紙送り実行
         /// </summary>
-        protected void SetPicaPitch()
+        /// <param name="value"></param>
+        protected void FeedPaper(int value)
         {
-            binaryWriter.Write((byte)0x1b);
-            binaryWriter.Write((byte)0x50);
+            if (0 <= value &&
+                value < 256)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x4a);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("FeedPaper: Bad Parameter: {0:d}", value));
+            }
         }
 
         /// <summary>
@@ -159,6 +375,126 @@ namespace mkbin_mw145
         {
             binaryWriter.Write((byte)0x1b);
             binaryWriter.Write((byte)0x4d);
+        }
+
+        /// <summary>
+        /// パイカピッチ指定
+        /// </summary>
+        protected void SetPicaPitch()
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x50);
+        }
+
+        /// <summary>
+        /// 英数カナ文字サイズ指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetAnkFontSize(int value)
+        {
+            if (value == 16 ||
+                value == 24 ||
+                value == 32 ||
+                value == 33 ||
+                value == 38 ||
+                value == 42 ||
+                value == 46 ||
+                value == 50 ||
+                value == 58 ||
+                value == 67 ||
+                value == 75 ||
+                value == 83 ||
+                value == 92 ||
+                value == 100 ||
+                value == 117 ||
+                value == 133 ||
+                value == 150 ||
+                value == 167 ||
+                value == 200 ||
+                value == 233 ||
+                value == 267 ||
+                value == 300 ||
+                value == 333 ||
+                value == 367 ||
+                value == 400)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x58);
+                binaryWriter.Write('0');
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetAnkFontSize: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 相対水平位置指定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetHorizontalReativePosition(int value)
+        {
+            if (0 <= value &&
+                value < 1180)
+            {
+                byte lb = (byte)(value % 255);
+                byte hb = (byte)(value / 256);
+
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x5c);
+                binaryWriter.Write(lb);
+                binaryWriter.Write(hb);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetHorizontalReativePosition: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        ///位置揃えの設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetAlignment(int value)
+        {
+            if (0 <= value &&
+                value < 4)
+            {
+                binaryWriter.Write((byte)0x1b);
+                binaryWriter.Write((byte)0x61);
+                binaryWriter.Write((byte)value);
+            }
+            else
+            {
+                throw new Exception(string.Format("SetAlignment: Bad Parameter: {0:d}", value));
+            }
+        }
+
+        /// <summary>
+        /// 左マージン設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetLeftMargin(int value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x6c);
+            binaryWriter.Write((byte)value);
+        }
+
+        /// <summary>
+        /// 右マージン設定
+        /// </summary>
+        /// <param name="value"></param>
+        protected void SetRightMargin(int value)
+        {
+            binaryWriter.Write((byte)0x1b);
+            binaryWriter.Write((byte)0x51);
+            binaryWriter.Write((byte)value);
         }
 
         /// <summary>
@@ -173,6 +509,7 @@ namespace mkbin_mw145
         /// <summary>
         /// プロポーショナル文字の選択
         /// </summary>
+        /// <param name="value"></param>
         protected void SetProportionalFont(bool value)
         {
             binaryWriter.Write((byte)0x1b);
@@ -190,6 +527,7 @@ namespace mkbin_mw145
         /// <summary>
         /// 倍幅拡大文字の選択
         /// </summary>
+        /// <param name="value"></param>
         protected void SetDoubleWidthFont(bool value)
         {
             binaryWriter.Write((byte)0x1b);
